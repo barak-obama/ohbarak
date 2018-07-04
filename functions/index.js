675cb5a08cffd1fd134f984e4853de3ca7112ba1
Barak Obama <barak.oh16@gmail.com>
2018-07-04 18:58:35 +0300

    first commit

---
+++ functions/index.js
@@ -0,0 +1,53 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+
+const serviceAccount = require("./ohbarak-42e3f-firebase-adminsdk-2num4-f8fe27238e.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://ohbarak-42e3f.firebaseio.com"
+});
+
+const fileBucket = "gs://ohbarak-42e3f.appspot.com";
+const database = admin.database();
+
+const storageBucket = admin.storage().bucket(fileBucket);
+
+const indexRouter = require('./routes/index')(database, storageBucket);
+
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+/* GET home page. */
+app.use('/', indexRouter);
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+exports.app = functions.https.onRequest(app);
+
+
